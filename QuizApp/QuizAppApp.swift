b98889b25e1a9e46b607252c79466f6f70f1a2a4
Erik <3kdapp25.norder@folkuniversitetet.nu>
2026-03-30 13:16:47 +0200

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Erik on 2026-03-30.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
