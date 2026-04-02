0f60530d95aaf2cde24276a21665e278816f10e0
Ahmad <ahmadalhayel20@gmail.com>
2026-04-03 01:46:22 +0200

    Fixed answer color logic using correctAnswer

--- QuizApp/CardView.swift
+++ QuizApp/CardView.swift
@@ -54,16 +54,13 @@ struct CardView: View {
         onAnswerSelected(answer)
 
     }
-
     func backgroundColor(for answer: String) -> Color {
         guard showResult else {
             return Color.blue.opacity(0.2)
         }
 
-        if let correctAnswer = card.answers.first {
-            if answer == correctAnswer {
-                return .green
-            }
+        if answer == card.correctAnswer {
+            return .green
         }
 
         if answer == selectedAnswer {
