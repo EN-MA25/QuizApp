39ddf08a7ff0440b622a4509d5ca09c7ee666729
Erik <3kdapp25.norder@folkuniversitetet.nu>
2026-04-02 17:13:49 +0200

    Moved ResultView to it´s seperate view

--- QuizApp/QuizAppApp.swift
+++ QuizApp/QuizAppApp.swift
@@ -44,28 +44,3 @@ struct QuizAppApp: App {
         }
     }
 }
-
-// Simple result placeholder
-struct ResultView: View {
-    @Binding var phase: AppPhase
-    let score: Int
-    let total: Int
-
-    var body: some View {
-        VStack(spacing: 16) {
-            Text("Resultat")
-                .font(.title)
-                .bold()
-            Text("Du fick \(score) av \(total) poäng")
-                .font(.body)
-            Button("Börja om") {
-                withAnimation {
-                    phase = .start
-                }
-
-            }
-            .buttonStyle(.bordered)
-        }
-        .padding()
-    }
-}
---
+++ QuizApp/ResultView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultView.swift
+//  QuizApp
+//
+//  Created by Erik on 2026-04-02.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultView()
+}
