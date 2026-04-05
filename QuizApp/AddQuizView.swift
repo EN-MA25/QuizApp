9ca2c50d8e9d03e07ca2d38503a045e78a5b09c0
Erik <3kdapp25.norder@folkuniversitetet.nu>
2026-04-05 21:45:59 +0200

    Added next button to go to next TextField while adding questions

--- QuizApp/AddQuizView.swift
+++ QuizApp/AddQuizView.swift
@@ -21,6 +21,16 @@ struct AddQuizView: View {
     
     @State private var cards: [Card] = []
     
+    @FocusState private var focusedField: Field?
+    
+    enum Field: Hashable {
+        case category
+        case question
+        case answer1
+        case answer2
+        case answer3
+        case answer4
+    }
     
     var body: some View {
         VStack {
@@ -29,26 +39,38 @@ struct AddQuizView: View {
                 .font(.body)
                 .padding(.horizontal, 4)
                 .frame(height: 35)
+                .focused($focusedField, equals: .category)
+                .submitLabel(.next)
             TextField("Fråga ...", text: $questionText)
                 .font(.body)
                 .padding(.horizontal, 4)
                 .frame(height: 35)
+                .focused($focusedField, equals: .question)
+                .submitLabel(.next)
             TextField("Svar 1 (Korrekta Svaret)", text: $answer1)
                 .font(.body)
                 .padding(.horizontal, 4)
                 .frame(height: 35)
+                .focused($focusedField, equals: .answer1)
+                .submitLabel(.next)
             TextField("Svar 2", text: $answer2)
                 .font(.body)
                 .padding(.horizontal, 4)
                 .frame(height: 35)
+                .focused($focusedField, equals: .answer2)
+                .submitLabel(.next)
             TextField("Svar 3", text: $answer3)
                 .font(.body)
                 .padding(.horizontal, 4)
                 .frame(height: 35)
+                .focused($focusedField, equals: .answer3)
+                .submitLabel(.next)
             TextField("Svar 4", text: $answer4)
                 .font(.body)
                 .padding(.horizontal, 4)
                 .frame(height: 35)
+                .focused($focusedField, equals: .answer4)
+                .submitLabel(.next)
             HStack(spacing: 40) {
                 Button("Spara") {
                     if !questionText.isEmpty, !answer1.isEmpty, !answer2.isEmpty, !answer3.isEmpty, !answer4.isEmpty {
@@ -60,6 +82,7 @@ struct AddQuizView: View {
                         answer2 = ""
                         answer3 = ""
                         answer4 = ""
+                        focusedField = .question
                         
                     }
                     
@@ -69,7 +92,25 @@ struct AddQuizView: View {
                 }
             }
         }
-    
+        .padding(20)
+        .onSubmit {
+            switch focusedField {
+            case .category:
+                focusedField = .question
+            case .question:
+                focusedField = .answer1
+            case .answer1:
+                focusedField = .answer2
+            case .answer2:
+                focusedField = .answer3
+            case .answer3:
+                focusedField = .answer4
+            case .answer4:
+                focusedField = nil
+            case .none:
+                break
+            }
+        }
     }
 }
 
